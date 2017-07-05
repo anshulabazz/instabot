b10a55df010332126aec1d2bb101e65a9d412148
Anshul Sharma <9882843092as@gmail.com>
2017-07-05 20:42:23 +0530

    to know the id of user

---
+++ get_user_id.py
@@ -0,0 +1,17 @@
+import requests
+from constant import APP_ACCESS_TOKEN, BASE_URL
+
+def get_user_id(insta_username):
+    #functions logic.
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print ('GET request url : %s' % (request_url))
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print( 'Status code other than 200 received!')
+        exit()
