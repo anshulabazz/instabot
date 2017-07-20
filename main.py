e9b08d85480118089c8c2900eaca84e8d7545c2d
Anshul Sharma <9882843092as@gmail.com>
2017-07-20 15:12:01 +0530

    (files modified)

--- main.py
+++ main.py
@@ -7,21 +7,20 @@ from like_user_post import like_user_post
 from comment_user_post import comment_user_post1
 from self_info import self_info
 
-
 from post_a_comment import post_a_comment
 from delete_negative_comment import delete_negative_comment
 import sys
 from termcolor import colored
 from trending_hashtag import get_trending_tag_counts
 
-
 #   <-------------------------InstaBot Application Starts From Here------------------------>
 
-color_text = colored("\n\t\t\t<------------*****Welcome To InstaBot Application*****------------>\n","blue")
-print (color_text)
+color_text = colored("\n\t\t\t<------------*****Welcome To InstaBot Application*****------------>\n", "blue")
+print
+color_text
 
 # Ask What you Want to do
-menu_choices = "What do you want to do. Select from the below Choices (1-6)" \
+choice = "What do you want to do. Select from the below Choices (1-6)" \
                "\n\n\t\t1. Show Own Profile Info " \
                "\n\t\t2. Download Own Post " \
                "\n\t\t3. Get User's Profile Information. " \
@@ -34,74 +33,78 @@ menu_choices = "What do you want to do. Select from the below Choices (1-6)" \
                "\n\t\t10. Close Application  "
 
 show_menu = True
-while show_menu:        #if user enter valid key
-    menu_choice = input(menu_choices)
-    if (menu_choice) > 0:
-        menu_choice = int(menu_choice)
+while show_menu:  # if user enter valid key
+    choice = input(choice)
+    choice = int(choice)
+    if choice == 1:
+        # Control Goes to self_info.py
+        self_info()
+        print("\n\n")
 
-        if menu_choice == 1:
-            # Control Goes to self_info.py
-            self_info()
-            print("\n\n")
+    elif choice == 2:
+        print("\n\t$$$$$ Downloading Your Own Recent Post.....\n")
+        # Control Goes to get_own_post.py
+        get_own_post()
+        print("\n")
 
-        elif menu_choice == 2:
-            print ("\n\t$$$$$ Downloading Your Own Recent Post.....\n")
-            # Control Goes to get_own_post.py
-            get_own_post()
-            print("\n")
+    elif choice == 3:
+        insta_username = input("\nEnter The Username Who's Information You Want. \n")
+        print("\t\tWait ** Information is Downloading...\n")
+        # Control Goes to get_user_info.py
+        get_user_info(insta_username)
+        print("\n")
 
-        elif menu_choice == 3:
-            insta_username =input("\nEnter The Username Who's Information You Want. \n")
-            print("\t\tWait ** Information is Downloading...\n")
-            # Control Goes to get_user_info.py
-            get_user_info(insta_username)
-            print("\n")
+    elif choice == 4:
+        insta_username = input("\nEnter The Username Who's Recent Post You Want To Download. \n")
+        print("\t\tWait ** User's Recent Post is Downloading...")
+        # Control Goes to get_user_post.py
+        get_user_post(insta_username)
+        print("\n")
 
-        elif menu_choice == 4:
-            insta_username =input("\nEnter The Username Who's Recent Post You Want To Download. \n")
-            print("\t\tWait ** User's Recent Post is Downloading...")
-            # Control Goes to get_user_post.py
-            get_user_post(insta_username)
-            print("\n")
+    elif choice == 5:
+        insta_username = input("Enter The Username Who's Recent Post You Want To Like. ")
+        print("Wait ** Liking The User's Recent Post...")
+        # Control Goes to like_user_post.py
+        like_user_post(insta_username)
+        print("\n")
 
-        elif menu_choice == 5:
-            insta_username =input("Enter The Username Who's Recent Post You Want To Like. ")
-            print("Wait ** Liking The User's Recent Post...")
-            # Control Goes to like_user_post.py
-            like_user_post(insta_username)
-            print("\n")
-
-        elif menu_choice == 6:
-            insta_username =input("\nEnter The Username Who's Recent Post You Want To Write Something. \n")
-            print("\t\tWait ** Commenting in the User's Recent Post...")
-            # Control Goes to comment_user_post.py
-            comment_user_post1(insta_username)
-            print("\n")
+    elif choice == 6:
+        insta_username = input("\nEnter The Username Who's Recent Post You Want To Write Something. \n")
+        print("\t\tWait ** Commenting in the User's Recent Post...")
+        # Control Goes to comment_user_post.py
+        comment_user_post1(insta_username)
+        print("\n")
 
 
-        elif menu_choice == 7:
-            insta_username =input("Enter the username of the user: ")
-            post_a_comment(insta_username)
+    elif choice == 7:
+        insta_username =input("Enter the username of the user: ")
+        post_a_comment(insta_username)
 
-        elif menu_choice == 8:
-            insta_username = input("Enter the username of the user: ")
-            delete_negative_comment(insta_username)
+    elif choice == 8:
+        insta_username =input("Enter the username of the user: ")
+        delete_negative_comment(insta_username)
 
-        elif menu_choice == 9:
-            tag =input("Enter Tagname.........\n")
-            print("Wait counting Ur Tags......\n")
-            get_trending_tag_counts(tag)
-            print("Style.RESET_ALL\n")
-            print("\n")
-            print("\n")
+    elif choice == 9:
+        tag = input("Enter Tagname.........\n")
+        print("Wait counting Ur Tags......\n")
+        get_trending_tag_counts(tag)
+        print("Style.RESET_ALL\n")
+        print("\n")
+        print("\n")
 
-        elif menu_choice == 10:
-            print( "\n\t\t<------------******Thanks To Be With Us*****------------>")
-            exit()
+    elif choice == 10:
+        print(
+            "\n\t\t<------------******Thanks To Be With Us*****------------>")
+        exit()
 
-        else:
-            print ("<------------*****Wrong Input*****------------>")
-            show_menu = False
+    else:
+        print(
+        "<------------*****Wrong Input*****------------>")
+        show_menu = False
+
+
+
 
 
 #   <-------------------------InstaBot Application Ends Here------------------------>
+
--- post_a_comment.py
+++ post_a_comment.py
@@ -1,19 +1,23 @@
-#Files and Functions are Imported
 import requests
-from constant import APP_ACCESS_TOKEN ,BASE_URL
-from get_user_post import get_user_post
+from constant import APP_ACCESS_TOKEN,BASE_URL
+from get_post_id import get_post_id
 
-def comment_user_post1(insta_username):
-    #Function Logic for post a Comment
-    post_id = get_user_post(insta_username)   #Get the User's Post Id
-    message =input("\n\tPlease Enter Your comment : \n")
-    request_url = (BASE_URL + 'media/%s/comments') % (post_id)
-    payload = {"access_token":APP_ACCESS_TOKEN, "text": message}
-    post_a_comment = requests.post(request_url, payload).json()
-    print ('POST request url : %s' % (request_url))
 
-    print(post_a_comment['meta']['code'])   #Check if Comment is Posted Or Not
-    if post_a_comment['meta']['code'] == 200:
-        print("\n\t\t\t*****Post comment successfully*****")
-    else :
-        print("\n\t\t\t*****Post comment Unsuccessfull*****")
+
+
+
+def post_a_comment(insta_username):
+  media_id = get_post_id(insta_username)
+  comment_text = input("Your comment: ")
+  payload = {"access_token": APP_ACCESS_TOKEN, "text" : comment_text}
+  request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+  print ('POST request url : %s' % (request_url))
+
+  make_comment = requests.post(request_url, payload).json()
+  if make_comment['meta']['code'] == 200:
+      print ("Successfully added a new comment!")
+  else:
+      print ("Unable to add comment. Try again!")
+
+
+
